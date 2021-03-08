07fa9e10571a06eebdef91ed3edbc81b81ff4e81
JNicoVM <jnvmud@gmail.com>
2021-03-08 12:33:20 -0500

    first commit

---
+++ app/src/main/java/com/example/valid_prueba/activities/SearchActivity.kt
@@ -0,0 +1,4 @@
+package com.example.valid_prueba.activities
+
+class SearchActivity {
+}
---
+++ app/src/main/java/com/example/valid_prueba/retrofit/APIConstants.kt
@@ -0,0 +1,5 @@
+package com.example.valid_prueba.retrofit
+
+object APIConstants {
+    const val BASE_API_URL = "https://api.mercadolibre.com/sites/MLA/"
+}
